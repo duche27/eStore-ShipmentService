bc9eef6702850a775872790204bad53f37deac3e
Guillermo Fernández <guillermo_fernandez_raboso@carrefour.com>
2022-04-20 10:58:46 +0200

    first commit

---
+++ src/test/java/com/gui/estore/ShipmentService/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.estore.ShipmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
